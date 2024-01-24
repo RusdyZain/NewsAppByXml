7fa575d816adf3722b21ed1c9bd5da4c6f340459
RusdyZain <ryanfajri28@gmail.com>
2024-01-24 18:27:33 +0800

    Retrofit Progress

---
+++ app/src/main/java/com/example/newsappbyxml/Source.kt
@@ -0,0 +1,6 @@
+package com.example.newsappbyxml
+
+data class Source(
+    val id: String,
+    val name: String
+)
---
+++ app/src/main/java/com/example/newsappbyxml/util/Constant.kt
@@ -0,0 +1,8 @@
+package com.example.newsappbyxml.util;
+
+class Constants {
+    companion object {
+        const val API_KEY = "756a22b18dca4093a94929c3fe8480f1"
+        const val BASE_URL = "https://newsapi.org"
+    }
+}
